1a4f5f9ebbe070e1837aab4453de47dbaccd7252
jordan69420 <58832117+jordan69420@users.noreply.github.com>
2021-07-24 19:23:55 -0400

    Update frkhash.cu

--- frankohash/lib/frankohash/frkhash.cu
+++ frankohash/lib/frankohash/frkhash.cu
@@ -4,7 +4,7 @@ int mine(const hash256* header_hash) {
    int nonce = 0;
    while (true) {
       //add condition for diff
-      if(frankohash_hash(header_hash, nonce)) {
+      if(frankohash_hash(header_hash, nonce)::final_hash) {
          return nonce;
          break;
       }
