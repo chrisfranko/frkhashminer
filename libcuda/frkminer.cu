20bf6f25506f0d252d1115f62f8cf19000ea2b6e
jordan69420 <58832117+jordan69420@users.noreply.github.com>
2021-07-24 21:27:58 -0400

    Update frkminer.cu

--- libcuda/frkminer.cu
+++ libcuda/frkminer.cu
@@ -1,8 +1,1 @@
-#include "frkhashminer/frankohash/lib/keccak/keccak.h";
 
-int mine(frankohash header, int diff) {
-   while(true) {
-      frankohash h = frankohash_keccak256(header, nonce)
-      if
-   }
-}
